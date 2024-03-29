d887a2104dd3f3bcf41cedff0347c4aa36d2b6d2
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2024-03-29 10:21:18 +0700

    fix: add metadata nft

--- nft/scripts/nft.ts
+++ nft/scripts/nft.ts
@@ -3,7 +3,7 @@ import hre, { ethers } from "hardhat";
 
 async function main() {
     const owner = "0x44F2DBd5439Ecb438d5c9d4B72ECA417C5898FcB"
-    const attributes = "0x96326B0C701bC83D6FA815F2F2C9374C0B6D7258"
+    const attributes = "0x5cFe1538bAd0a4835A8DE936246CC9960E91D209"
     const name = "Dwarves Foundation"
     const symbol = "DF"
     const nfts = await ethers.getContractFactory('Nft');
@@ -11,7 +11,7 @@ async function main() {
     const nft = await nfts.deploy(name, symbol, owner, attributes);
     await nft.waitForDeployment()
     console.log('NFT Contract Deployed at ' + nft.target);
-    //0xc2FBBB7f68Afe47be2C32631682b065222d597b1
+    //0xb52c1e15815268beBe55A6C8CAC886831FD75cdA
 }
 
 // We recommend this pattern to be able to use async/await everywhere
