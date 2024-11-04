1a0cf859213cf49ecefd53f3de5dc72e5b1b284b
phucledien <phucledien@gmail.com>
2024-11-05 06:41:27 +0700

    chore: initial icy btc

---
+++ icy-btc/src/IcyBtcSwap.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract IcyBtcSwap {
+    address public signerAddress;
+
+    event Swap(uint256 amount, string btcAddress, string btcAmount);
+    event SetSigner(address signerAddress);
+
+    fallback() external {
+        revert();
+    }
+
+    receive() external payable {
+        revert();
+    }
+
+    // =========================== VIEW FUNCTIONS ============================ //
+    function getSigner(
+        bytes32 _digest,
+        bytes memory _signature
+    ) public pure returns (address) {
+        return ECDSA.recover(_digest, _signature);
+    }
+
+    // =========================== CALL FUNCTIONS ============================ //
+    function setSigner(address _signerAddress) external onlyOwner {
+        signerAddress = _signerAddress;
+        emit SetSigner(signerAddress);
+    }
+}
